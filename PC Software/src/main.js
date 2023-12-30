42a4069d8e2bb8fc6afaa4f6dd971bc2cade2005
Nikola Bojinović <bojinovicnikola12@gmail.com>
2023-12-30 19:01:05 +0100

    Update main.js
    - Added Warning function
    - Added Serial communication

--- PC Software/src/main.js
+++ PC Software/src/main.js
@@ -1,3 +1,5 @@
+const { SerialPort, ReadlineParser } = require("serialport");
+
 const keypadBox = document.querySelector(".keypad-box");
 
 const keyNameInput = document.getElementById("keyName");
@@ -5,81 +7,125 @@ const keyStrokeInput = document.getElementById("keyStroke");
 const textInput = document.getElementById("inputText");
 const saveButton = document.getElementById("save");
 
+const warnBox = document.getElementById("warn-box");
+
+const portElement = document.getElementById("portAndPath");
+
 let key_insert = false;
 
 let btnId;
 
-const strokes = []
+const strokes = [];
 
 let i = 0;
 
-const toggleInsertTrue = () => {
-    key_insert = true;
-    document.body.classList.replace("key_insert_false", "key_insert_true");
-}
+const RP2040_ZERO_VENDOR_ID = "2E8A";
 
-const toggleInsertFalse = () => {
-    key_insert = false;
-    document.body.classList.replace("key_insert_true", "key_insert_false");
-    emptyStrokes();
-}
+SerialPort.list().then(boards => {
+    console.log(boards[0]);
 
-const toggleInsert = (button) => {
-    if (button.id != "" && !key_insert) {
-        toggleInsertTrue();
-    } else if (button.id != "" && key_insert) {
-        toggleInsertFalse();
+    if (boards[0].vendorId == RP2040_ZERO_VENDOR_ID) {
+        portElement.innerHTML = `RP2040 - ${boards[0].path}`;
+
+        const port = new SerialPort({
+            path: boards[0].path,
+            baudRate: 9600,
+            dataBits: 8,
+            parity: "none",
+            stopBits: 1,
+            autoOpen: false
+        });
+
+        const parser = new ReadlineParser({ delimiter: "\r\n" });
+        port.pipe(parser);
+
+        console.log(`${port.path} is open at baud rate: ${port.baudRate}`);
+
+        const toggleInsertTrue = () => {
+            key_insert = true;
+            document.body.classList.replace("key_insert_false", "key_insert_true");
+        }
+        
+        const toggleInsertFalse = () => {
+            key_insert = false;
+            document.body.classList.replace("key_insert_true", "key_insert_false");
+            emptyStrokes();
+        }
+        
+        const toggleInsert = (button) => {
+            if (button.id != "" && !key_insert) {
+                toggleInsertTrue();
+            } else if (button.id != "" && key_insert) {
+                toggleInsertFalse();
+            }
+        }
+        
+        const emptyStrokes = () => {
+            for (i; i >= 0; i--) {
+                strokes[i] = "";
+            }
+        
+            i = 0;
+        
+            console.log(`strokes are empty: ${strokes[0]} + ${strokes[1]} + ${strokes[2]}`);
+        
+            keyNameInput.value = "";
+            keyStrokeInput.value = "";
+            textInput.value = "";
+            keyStrokeInput.classList.remove("warning");
+            warnBox.classList.replace("warning-box-true", "warning-box-false");
+        }
+        
+        keypadBox.addEventListener("click", e => {
+            let button = e.target;
+            btnId = button.id;
+            toggleInsert(button);
+        });
+        
+        window.addEventListener("keydown", e => {
+            if (e.key == "Escape" && key_insert) toggleInsertFalse();
+        })
+        
+        keyStrokeInput.addEventListener("keyup", e => {
+            if (i < 3) {
+                if (keyStrokeInput.value == "") keyStrokeInput.value = e.key;
+                else {
+                    if (e.key != "Control" && e.key != "Alt" && e.key != "Shift") keyStrokeInput.value += ` + ${e.key.toUpperCase()}`;
+                    else keyStrokeInput.value += ` + ${e.key}`;
+                }
+                strokes[i] = e.key;
+                i++;
+            } else {
+                console.log(`Strokes are full : ${strokes[0]} + ${strokes[1]} + ${strokes[2]}`);
+                keyStrokeInput.classList.add("warning");
+                warnBox.classList.replace("warning-box-false", "warning-box-true");
+                return;
+            }
+        })
+        
+        keyStrokeInput.addEventListener("dblclick", emptyStrokes);
+        
+        saveButton.addEventListener("click", () => {
+            if (keyNameInput.value == "" && keyStrokeInput.value == "" && textInput.value == "") toggleInsertFalse();
+            else {
+                console.log(`Key Name: ${keyNameInput.value} | Key Stroke: ${keyStrokeInput.value} | Text: ${textInput.value} | Button ID: ${btnId}`);
+                document.getElementById(btnId).innerHTML = keyNameInput.value[0];
+                
+                //port.write(`ks:${keyStrokeInput.value}\n`);
+                port.write(`txt:${textInput.value}\n`);
+                //port.write(`btn:${btnId}\n`);
+
+                keyNameInput.value = "";
+                keyStrokeInput.value = "";
+                textInput.value = "";
+            
+                toggleInsertFalse();
+            }
+        })
+
+        parser.on("data", data => {
+            console.log(`data: ${data}`);
+        });
     }
-}
-
-const emptyStrokes = () => {
-    for (i; i >= 0; i--) {
-        strokes[i] = "";
-    }
-
-    i = 0;
-
-    console.log(`strokes are empty: ${strokes[0]} + ${strokes[1]} + ${strokes[2]}`);
-
-    keyNameInput.value = "";
-    keyStrokeInput.value = "";
-    textInput.value = "";
-}
-
-keypadBox.addEventListener("click", e => {
-    let button = e.target;
-    btnId = button.id;
-    toggleInsert(button);
 });
 
-window.addEventListener("keydown", e => {
-    if (e.key == "Escape" && key_insert) toggleInsertFalse();
-})
-
-keyStrokeInput.addEventListener("keyup", e => {
-    if (i < 3) {
-        if (keyStrokeInput.value == "") keyStrokeInput.value = e.key;
-        else {
-            if (e.key != "Control" && e.key != "Alt" && e.key != "Shift") keyStrokeInput.value += ` + ${e.key.toUpperCase()}`;
-            else keyStrokeInput.value += ` + ${e.key}`;
-        }
-        strokes[i] = e.key;
-        i++;
-    } else {
-        console.log(`Strokes are full : ${strokes[0]} + ${strokes[1]} + ${strokes[2]}`);
-        return;
-    }
-
-})
-
-saveButton.addEventListener("click", () => {
-    if (keyNameInput.value == "" && keyStrokeInput.value == "" && textInput.value == "") toggleInsertFalse();
-    else {
-        console.log(`Key Name: ${keyNameInput.value} | Key Stroke: ${keyStrokeInput.value} | Text: ${textInput.value} | Button ID: ${btnId}`);
-        keyNameInput.value = "";
-        keyStrokeInput.value = "";
-        textInput.value = "";
-    
-        toggleInsertFalse();
-    }
-})
